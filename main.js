8eebe66fdb65fbba75989864138043b7762f7793
dlsso <supertonesfan@hotmail.com>
2014-06-27 10:52:35 -0600

    Append with .each

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $(document).on('ready', function() {
   
+  	// Loop over each 'li'
+	$('li').each(function(index, domElement){
+
+		// Append some exclamation points to the li
+		// that is current in the loop
+		$(this).append('!!')
+
+
+
+	})
+
+
+
 });
